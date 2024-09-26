8d63bb2adb8b20a8dd60badd468fba0563487237
Ariska Hidayat <ariska138@gmail.com>
2024-09-26 13:15:05 +0700

    adhan

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('program utama saya');
+const adhan = require('adhan');
+
+const coordinates = new adhan.Coordinates(35.7897507, -78.6912485);
+const params = adhan.CalculationMethod.MoonsightingCommittee();
+const date = new Date();
+
+const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+
+console.log(prayerTimes);
+
